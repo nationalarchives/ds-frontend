c686eb54ad3d78688fe115a7a1d667510774f916
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-01-16 14:06:59 +0000

    First pass at new menu

--- app/main/routes.py
+++ app/main/routes.py
@@ -18,3 +18,9 @@ def cookies():
 @bp.route("/healthcheck/live/")
 def healthcheck():
     return "ok"
+
+
+@bp.route("/menu/")
+@cache.cached()
+def menu():
+    return render_template("main/new_menu.html")
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -6,3 +6,55 @@ import {
 window.TNAFrontend = { Cookies };
 
 initAll();
+
+/*
+ * ==========================================
+ * TEMP: NEW HEADER
+ * ==========================================
+ */
+document.querySelectorAll(".tna-new-header").forEach((header) => {
+  const $menuButtonWrapper = header.querySelector(
+    ".tna-new-header__menu-button-wrapper",
+  );
+  const $menuButton = document.createElement("button");
+  $menuButton.classList.add("tna-new-header__menu-button");
+  $menuButton.innerText = "Menu";
+  $menuButton.addEventListener("click", () => {
+    console.log("click");
+  });
+  $menuButtonWrapper.appendChild($menuButton);
+  const $menuNavigationDisclosure = header.querySelector(
+    ".tna-new-header__navigation-disclosure",
+  );
+  const $menuNavigationItemTitleLinks = header.querySelectorAll(
+    ".tna-new-header__navigation-item-link",
+  );
+  $menuNavigationItemTitleLinks.forEach(($itemTitleLink) => {
+    const $item = $itemTitleLink.closest(".tna-new-header__navigation-item");
+    if ($item) {
+      const $itemContents = $item.querySelector(
+        ".tna-new-header__navigation-item-contents",
+      );
+      if ($itemContents) {
+        $itemContents.setAttribute("hidden", true);
+        $itemTitleLink.addEventListener("click", (e) => {
+          e.preventDefault();
+          $menuNavigationDisclosure.innerHTML = $item.innerHTML;
+          const $itemContentsCopy = $menuNavigationDisclosure.querySelector(
+            ".tna-new-header__navigation-item-contents[hidden]",
+          );
+          if ($itemContentsCopy) {
+            $itemContentsCopy.removeAttribute("hidden");
+          }
+          const $closeDisclosure = document.createElement("button");
+          $closeDisclosure.innerText = "Close";
+          $closeDisclosure.addEventListener("click", () => {
+            $menuNavigationDisclosure.innerHTML = "";
+          });
+          $menuNavigationDisclosure.append($closeDisclosure);
+          $menuNavigationDisclosure.focus();
+        });
+      }
+    }
+  });
+});
