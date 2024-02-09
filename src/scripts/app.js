205d4e80568dbfd1e63ce10ca0e598175d8e2822
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-02-09 10:54:32 +0000

    Add draft analytics

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,43 +1,98 @@
-import {
-  initAll,
-  Cookies,
-} from "@nationalarchives/frontend/nationalarchives/all.mjs";
-
-window.TNAFrontend = { Cookies };
+import { initAll } from "@nationalarchives/frontend/nationalarchives/all.mjs";
+// import {GA4} from "@nationalarchives/frontend/nationalarchives/analytics.mjs";
 
 initAll();
+
+/*
+ * ==========================================
+ * TEMP: ANALYTICS
+ * ==========================================
+ */
+// const TNAAnalytics = window.TNAFrontendAnalytics
+// const analytics = new TNAAnalytics.GA4("GTM-KX8ZWVZG");
+
+// analytics.addListener(".etna-article__sidebar", "sidebar", [
+//   {
+//     eventName: "scection_jump",
+//     targetElement: ".etna-article__sidebar-item",
+//     on: "click",
+//     data: {
+//       value: TNAAnalytics.helpers.valueGetters.text,
+//     },
+//   },
+// ]);
+
+// analytics.addListener(".etna-article", "article", [
+//   {
+//     eventName: "toggle_section",
+//     targetElement: ".etna-article__section-button",
+//     on: "click",
+//     data: {
+//       // eslint-disable-next-line no-unused-vars
+//       state: ($el, $scope, event) => {
+//         const expanded = $el.getAttribute("aria-expanded");
+//         if (expanded === null) {
+//           return null;
+//         }
+//         return expanded.toString() === "true" ? "opened" : "closed";
+//       },
+//       value: TNAAnalytics.helpers.valueGetters.text,
+//     },
+//   },
+// ]);
+
+// analytics.addListener(document.documentElement, "doc", [
+//   {
+//     eventName: "double_click",
+//     on: "dblclick",
+//     data: {
+//       state: ($el, $scope, event) => getXPathTo(event.target),
+//       value: ($el, $scope, event) => event.target.innerHTML,
+//     },
+//   },
+// ]);
+
+// analytics.addListener(document.getElementById("tna-form__search"), "search", [
+//   {
+//     eventName: "search_term_blur",
+//     on: "blur",
+//     data: {
+//       value: TNAAnalytics.helpers.valueGetters.value,
+//     },
+//   },
+// ]);
 
 /*
  * ==========================================
  * TEMP: NEW HEADER
  * ==========================================
  */
-document.querySelectorAll(".tna-new-header").forEach((header) => {
+document.querySelectorAll(".tna-global-header").forEach((header) => {
   const $menuButtonWrapper = header.querySelector(
-    ".tna-new-header__menu-button-wrapper",
+    ".tna-global-header__menu-button-wrapper",
   );
   const $menuButton = document.createElement("button");
-  $menuButton.classList.add("tna-new-header__menu-button");
+  $menuButton.classList.add("tna-global-header__menu-button");
   $menuButton.innerText = "Menu";
   $menuButton.addEventListener("click", () => {
     console.log("click");
   });
   $menuButtonWrapper.appendChild($menuButton);
   const $menuNavigationDisclosure = header.querySelector(
-    ".tna-new-header__disclosure",
+    ".tna-global-header__disclosure",
   );
   const $menuNavigationItemTitleLinks = header.querySelectorAll(
-    ".tna-new-header__navigation-item-link",
+    ".tna-global-header__navigation-item-link",
   );
   $menuNavigationItemTitleLinks.forEach(($itemTitleLink) => {
-    const $item = $itemTitleLink.closest(".tna-new-header__navigation-item");
+    const $item = $itemTitleLink.closest(".tna-global-header__navigation-item");
     if ($item) {
       const $itemContents = $item.querySelector(
-        ".tna-new-header__navigation-item-contents",
+        ".tna-global-header__navigation-item-contents",
       );
 
       const $itemChildren = $item.querySelector(
-        ".tna-new-header__navigation-item-children",
+        ".tna-global-header__navigation-item-children",
       );
       if ($itemChildren) {
         $itemChildren.setAttribute("hidden", true);
@@ -57,30 +112,30 @@ document.querySelectorAll(".tna-new-header").forEach((header) => {
 
           $menuNavigationDisclosure.innerHTML = $item.innerHTML;
           const $itemContentsCopy = $menuNavigationDisclosure.querySelector(
-            ".tna-new-header__navigation-item-contents[hidden]",
+            ".tna-global-header__navigation-item-contents[hidden]",
           );
           if ($itemContentsCopy) {
             $itemContentsCopy.removeAttribute("hidden");
           }
           const $itemChildrenCopy = $menuNavigationDisclosure.querySelector(
-            ".tna-new-header__navigation-item-children[hidden]",
+            ".tna-global-header__navigation-item-children[hidden]",
           );
           if ($itemChildrenCopy) {
             $itemChildrenCopy.removeAttribute("hidden");
           }
           const $itemHeaderCopy = $menuNavigationDisclosure.querySelector(
-            ".tna-new-header__navigation-item-heading",
+            ".tna-global-header__navigation-item-heading",
           );
           if ($itemHeaderCopy) {
             $itemHeaderCopy.classList.remove(
-              "tna-new-header__navigation-item-heading",
+              "tna-global-header__navigation-item-heading",
             );
             $itemHeaderCopy.classList.add("tna-heading-l");
           }
           const $closeDisclosure = document.createElement("button");
           $closeDisclosure.innerText = "Close";
           $closeDisclosure.classList.add(
-            "tna-new-header__disclosure-close-button",
+            "tna-global-header__disclosure-close-button",
           );
           $closeDisclosure.addEventListener("click", () => {
             $menuNavigationDisclosure.innerHTML = "";
