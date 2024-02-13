5ff44d17ec3df30cbe464603a01e5e5cc6ff6ec4
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-02-13 18:11:14 +0000

    Update TNA Frontend and TNA Frontend Jinja

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -8,59 +8,59 @@ initAll();
  * TEMP: ANALYTICS
  * ==========================================
  */
-// const TNAAnalytics = window.TNAFrontendAnalytics
-// const analytics = new TNAAnalytics.GA4("GTM-KX8ZWVZG");
+const TNAAnalytics = window.TNAFrontendAnalytics;
+const analytics = new TNAAnalytics.GA4("GTM-KX8ZWVZG");
 
-// analytics.addListener(".etna-article__sidebar", "sidebar", [
-//   {
-//     eventName: "scection_jump",
-//     targetElement: ".etna-article__sidebar-item",
-//     on: "click",
-//     data: {
-//       value: TNAAnalytics.helpers.valueGetters.text,
-//     },
-//   },
-// ]);
+analytics.addListener(".etna-article__sidebar", "sidebar", [
+  {
+    eventName: "scection_jump",
+    targetElement: ".etna-article__sidebar-item",
+    on: "click",
+    data: {
+      value: TNAAnalytics.helpers.valueGetters.text,
+    },
+  },
+]);
 
-// analytics.addListener(".etna-article", "article", [
-//   {
-//     eventName: "toggle_section",
-//     targetElement: ".etna-article__section-button",
-//     on: "click",
-//     data: {
-//       // eslint-disable-next-line no-unused-vars
-//       state: ($el, $scope, event) => {
-//         const expanded = $el.getAttribute("aria-expanded");
-//         if (expanded === null) {
-//           return null;
-//         }
-//         return expanded.toString() === "true" ? "opened" : "closed";
-//       },
-//       value: TNAAnalytics.helpers.valueGetters.text,
-//     },
-//   },
-// ]);
+analytics.addListener(".etna-article", "article", [
+  {
+    eventName: "toggle_section",
+    targetElement: ".etna-article__section-button",
+    on: "click",
+    data: {
+      // eslint-disable-next-line no-unused-vars
+      state: ($el, $scope, event) => {
+        const expanded = $el.getAttribute("aria-expanded");
+        if (expanded === null) {
+          return null;
+        }
+        return expanded.toString() === "true" ? "opened" : "closed";
+      },
+      value: TNAAnalytics.helpers.valueGetters.text,
+    },
+  },
+]);
 
-// analytics.addListener(document.documentElement, "doc", [
-//   {
-//     eventName: "double_click",
-//     on: "dblclick",
-//     data: {
-//       state: ($el, $scope, event) => getXPathTo(event.target),
-//       value: ($el, $scope, event) => event.target.innerHTML,
-//     },
-//   },
-// ]);
+analytics.addListener(document.documentElement, "doc", [
+  {
+    eventName: "double_click",
+    on: "dblclick",
+    data: {
+      state: ($el, $scope, event) => getXPathTo(event.target),
+      value: ($el, $scope, event) => event.target.innerHTML,
+    },
+  },
+]);
 
-// analytics.addListener(document.getElementById("tna-form__search"), "search", [
-//   {
-//     eventName: "search_term_blur",
-//     on: "blur",
-//     data: {
-//       value: TNAAnalytics.helpers.valueGetters.value,
-//     },
-//   },
-// ]);
+analytics.addListener(document.getElementById("tna-form__search"), "search", [
+  {
+    eventName: "search_term_blur",
+    on: "blur",
+    data: {
+      value: TNAAnalytics.helpers.valueGetters.value,
+    },
+  },
+]);
 
 /*
  * ==========================================
