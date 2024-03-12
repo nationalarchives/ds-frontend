d5a593b2c6b80eca03c4abc775774782cf24d23b
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-03-12 14:10:27 +0000

    Add topics and time periods to article pages

--- app/wagtail/pages/article_page.py
+++ app/wagtail/pages/article_page.py
@@ -12,10 +12,36 @@ def article_page(page_data):
         ]
     except ConnectionError:
         return render_template("errors/api.html"), 502
+    topics = [{
+        'href': topic['url'],
+        'label': 'Topic',
+        'title': topic['title'],
+        'src': topic['teaser_image_jpeg']['full_url'],
+        'alt': topic['teaser_image_jpeg']['alt'],
+        'width': topic['teaser_image_jpeg']['width'],
+        'height': topic['teaser_image_jpeg']['height']
+    } for topic in page_data['topics']]
+    time_periods = [{
+        'href': time_period['url'],
+        'label': 'Time period',
+        'title': time_period['title'],
+        'src': time_period['teaser_image_jpeg']['full_url'],
+        'alt': time_period['teaser_image_jpeg']['alt'],
+        'width': time_period['teaser_image_jpeg']['width'],
+        'height': time_period['teaser_image_jpeg']['height']
+    } for time_period in page_data['time_periods']]
+    highlights = []
+    if topics and time_periods:
+        highlights = [topics[0], time_periods[0]]
+    elif topics:
+        highlights = [topics[0], topics[1]]
+    elif time_periods:
+        highlights = [time_periods[0], time_periods[1]]
     return render_template(
         "explore/article.html",
         breadcrumbs=breadcrumbs(page_data["id"]),
         page_data=page_data,
+        highlights=highlights,
         similar_items=similar_items,
         latest_items=latest_items,
     )
