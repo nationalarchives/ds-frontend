199c0d2fabee53457cf40b0b92d147dd6f6787ed
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-03-25 18:33:23 +0000

    No extra lookups for authors index page

--- app/wagtail/pages/author_index_page.py
+++ app/wagtail/pages/author_index_page.py
@@ -4,16 +4,19 @@ from flask import render_template
 
 def author_index_page(page_data):
     try:
-        # children_data = page_children(page_data["id"], {"fields": "_,title,teaser_image_jpg,role"})
-        children_data = page_children(page_data["id"], {"order": "title"})
-        children = [
-            page_details(child["id"]) for child in children_data["items"]
-        ]
+        children = page_children(
+            page_data["id"],
+            {
+                "type": "authors.AuthorPage",
+                "fields": "_,title,teaser_image_jpg,role,html_url",
+                "order": "title",
+            },
+        )
     except ConnectionError:
         return render_template("errors/api.html"), 502
     return render_template(
         "authors/index.html",
         breadcrumbs=breadcrumbs(page_data["id"]),
-        children=children,
+        children=children["items"],
         page_data=page_data,
     )
