db3bcbc87af68198e5426762cd98724b82e46847
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-06-12 17:58:36 +0100

    Add basic cookie preference page

--- app/legal/routes.py
+++ app/legal/routes.py
@@ -1,6 +1,10 @@
+import json
+from urllib.parse import quote, unquote
+
 from app.legal import bp
 from app.lib import cache, cache_key_prefix
-from flask import render_template
+from app.lib.util import strtobool
+from flask import make_response, render_template, request
 
 
 @bp.route("/")
@@ -9,9 +13,36 @@ def index():
     return render_template("legal/index.html")
 
 
-@bp.route("/cookies/")
-@cache.cached(key_prefix=cache_key_prefix)
+@bp.route("/cookies/", methods=["GET", "POST"])
 def cookies():
+    if request.method == "POST":
+        current_cookies_policy = {
+            "usage": False,
+            "settings": False,
+            "essential": True,
+        }
+        if "cookies_policy" in request.cookies:
+            cookies_policy = request.cookies["cookies_policy"]
+            current_cookies_policy = json.loads(unquote(cookies_policy))
+        new_cookies_policy = {
+            "usage": (
+                strtobool(request.form["usage"])
+                if "usage" in request.form
+                else current_cookies_policy["usage"]
+            ),
+            "settings": (
+                strtobool(request.form["settings"])
+                if "settings" in request.form
+                else current_cookies_policy["settings"]
+            ),
+            "essential": True,
+        }
+        response = make_response(render_template("legal/cookies.html"))
+        response.set_cookie(
+            "cookies_policy",
+            quote(json.dumps(new_cookies_policy, separators=(",", ":"))),
+        )
+        return response
     return render_template("legal/cookies.html")
 
 
