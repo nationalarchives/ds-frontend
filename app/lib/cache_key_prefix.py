340d972a09cb2c8271b22948b6dad0d7705db857
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-06-12 19:46:25 +0100

    Update cache key prefix

--- app/lib/cache_key_prefix.py
+++ app/lib/cache_key_prefix.py
@@ -3,4 +3,4 @@ from flask import request
 
 def cache_key_prefix():
     """Make a key that includes GET parameters."""
-    return f"{request.full_path}{request.cookies.get('cookie_preferences_set')}{request.cookies.get('theme')}"
+    return f"{request.full_path}{request.cookies.get('cookie_preferences_set' or '')}{request.cookies.get('theme' or '')}"
