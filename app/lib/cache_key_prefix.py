654b60ff36d817a8d4036233836eeed9eb10d0c1
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-06-12 19:45:27 +0100

    Add theme to cache key prefix

--- app/lib/cache_key_prefix.py
+++ app/lib/cache_key_prefix.py
@@ -3,5 +3,4 @@ from flask import request
 
 def cache_key_prefix():
     """Make a key that includes GET parameters."""
-    return f"{request.full_path}{request.cookies.get('cookie_preferences_set')}"
-    # return f"{request.full_path}{request.cookies.get("cookie_preferences_set")}{request.cookies.get("cookies_policy")}"
+    return f"{request.full_path}{request.cookies.get('cookie_preferences_set')}{request.cookies.get('theme')}"
