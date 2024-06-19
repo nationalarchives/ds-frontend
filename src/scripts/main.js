d3cdec1ecc0397f1d7b551b875c1fc2efe8ec2c5
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-06-19 18:00:35 +0100

    Define cookie domain without using the cookie banner

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
-import { initAll } from "@nationalarchives/frontend/nationalarchives/all.mjs";
+import {
+  initAll,
+  Cookies,
+} from "@nationalarchives/frontend/nationalarchives/all.mjs";
+
+const cookiesDomain =
+  document.documentElement.getAttribute("data-cookiesdomain");
+if (cookiesDomain) {
+  new Cookies({ domain: cookiesDomain });
+}
 
 initAll();
