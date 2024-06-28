7ea187649cd0e40fab03f59a327f2d5dadcb2103
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-06-28 11:43:48 +0100

    Add sidebar of siblings

--- app/wagtail/pages/general_page.py
+++ app/wagtail/pages/general_page.py
@@ -1,10 +1,14 @@
-from app.wagtail.api import breadcrumbs
+from app.wagtail.api import breadcrumbs, page_children
 from flask import render_template
 
 
 def general_page(page_data):
+    siblings = []
+    if True or page_data["showSiblings"]:
+        siblings = page_children(page_data["meta"]["parent"]["id"])
     return render_template(
         "main/general.html",
         breadcrumbs=breadcrumbs(page_data["id"]),
         page_data=page_data,
+        siblings=siblings["items"],
     )
