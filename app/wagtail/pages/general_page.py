781623b77cbc6690dad2873ab8879273da246905
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-07-17 13:09:06 +0100

    Don't error on general pages when they fail to get a list of sibling pages

--- app/wagtail/pages/general_page.py
+++ app/wagtail/pages/general_page.py
@@ -4,22 +4,23 @@ from flask import current_app, render_template
 
 def general_page(page_data):
     siblings = []
-    if True or page_data["showSiblings"]:
+    if True or page_data["show_siblings"]:
         try:
-            siblings = page_children(page_data["meta"]["parent"]["id"])
+            sibling_items = page_children(page_data["meta"]["parent"]["id"])
+            siblings = sibling_items["items"]
         except ConnectionError:
             current_app.logger.error(
                 f"API error getting children for page {page_data['id']}"
             )
-            return render_template("errors/api.html"), 502
+            # return render_template("errors/api.html"), 502
         except Exception:
             current_app.logger.error(
                 f"Exception getting children for page {page_data['id']}"
             )
-            return render_template("errors/server.html"), 500
+            # return render_template("errors/server.html"), 500
     return render_template(
         "main/general.html",
         breadcrumbs=breadcrumbs(page_data["id"]),
         page_data=page_data,
-        siblings=siblings["items"],
+        siblings=siblings,
     )
