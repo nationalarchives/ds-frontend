8d060406239e449edd4e21272a297c82d578b91f
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-07-23 16:09:48 +0100

    List children on hub pages if no links are defined

--- app/wagtail/pages/hub_page.py
+++ app/wagtail/pages/hub_page.py
@@ -3,21 +3,24 @@ from flask import current_app, render_template
 
 
 def hub_page(page_data):
-    try:
-        children = page_children(page_data["id"])
-    except ConnectionError:
-        current_app.logger.error(
-            f"API error getting children for page {page_data['id']}"
-        )
-        return render_template("errors/api.html"), 502
-    except Exception:
-        current_app.logger.error(
-            f"Exception getting children for page {page_data['id']}"
-        )
-        return render_template("errors/server.html"), 500
+    children = []
+    if not page_data["links"]:
+        try:
+            all_children = page_children(page_data["id"])
+            children = all_children["items"]
+        except ConnectionError:
+            current_app.logger.error(
+                f"API error getting children for page {page_data['id']}"
+            )
+            return render_template("errors/api.html"), 502
+        except Exception:
+            current_app.logger.error(
+                f"Exception getting children for page {page_data['id']}"
+            )
+            return render_template("errors/server.html"), 500
     return render_template(
         "main/hub.html",
         breadcrumbs=breadcrumbs(page_data["id"]),
         page_data=page_data,
-        children=children["items"],
+        children=children,
     )
