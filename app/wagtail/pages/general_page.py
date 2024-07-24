66b917f1a2a5843af479a977643b74be1e69ccd1
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-07-24 11:16:39 +0100

    Add pydash and make general page sidebars more resiliant in preview mode

--- app/wagtail/pages/general_page.py
+++ app/wagtail/pages/general_page.py
@@ -1,23 +1,26 @@
 from app.wagtail.api import breadcrumbs, page_children
 from flask import current_app, render_template
+from pydash import objects
 
 
 def general_page(page_data):
     siblings = []
-    if True or page_data["show_siblings"]:
+    if (True or page_data["show_siblings"]) and objects.get(
+        page_data, "meta.parent.id"
+    ):
         try:
             sibling_items = page_children(page_data["meta"]["parent"]["id"])
-            siblings = sibling_items["items"]
+            siblings = (
+                sibling_items["items"] if "items" in sibling_items else []
+            )
         except ConnectionError:
             current_app.logger.error(
                 f"API error getting children for page {page_data['id']}"
             )
-            # return render_template("errors/api.html"), 502
         except Exception:
             current_app.logger.error(
                 f"Exception getting children for page {page_data['id']}"
             )
-            # return render_template("errors/server.html"), 500
     return render_template(
         "main/general.html",
         breadcrumbs=breadcrumbs(page_data["id"]),
