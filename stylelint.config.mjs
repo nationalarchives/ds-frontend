bff5735aaf2130d50aee6f90f1c543aec0f29750
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-08-14 12:44:34 +0100

    Ignore stylelint for ie.scss

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,5 +1,5 @@
 import data from "./node_modules/@nationalarchives/frontend/config/stylelint.config.js";
 
-data.ignoreFiles = ["app/**/*.css", "tmp/**/*"];
+data.ignoreFiles = ["app/**/*.css", "/app/src/styles/ie.scss", "tmp/**/*"];
 
 export default data;
