4e196ad0e1958aadd6696988630849a74c5864ef
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-08-28 11:23:04 +0100

    Add section_tabs layout to general pages

--- src/scripts/sidebar.js
+++ src/scripts/sidebar.js
@@ -2,9 +2,104 @@ import SectionsSidebarHighlighter from "./lib/sections-sidebar";
 
 const $level2Headings = document
   .getElementById("page-body")
-  .querySelectorAll("h2[id]");
+  ?.querySelectorAll("h2[id]");
 const $sidebarItems = document
   .getElementById("page-sidebar")
-  .querySelectorAll(".tna-sidebar__item");
+  ?.querySelectorAll(".tna-sidebar__item");
+if ($level2Headings && $sidebarItems) {
+  new SectionsSidebarHighlighter($level2Headings, $sidebarItems);
+}
 
-new SectionsSidebarHighlighter($level2Headings, $sidebarItems);
+const $secondaryNavigation = document.getElementById("secondary-navigation");
+const $pageBody = document.getElementById("page-body");
+if ($secondaryNavigation && $pageBody) {
+  $secondaryNavigation.removeAttribute("hidden");
+  let selectedIndex = 0;
+  const sections = Array.from(
+    $secondaryNavigation.querySelectorAll("li:has(button[aria-controls])"),
+  ).map(($pageSectionItem) => {
+    const $pageSectionItemLink = $pageSectionItem.querySelector("button");
+    const id = $pageSectionItemLink.getAttribute("aria-controls");
+    return {
+      listItem: $pageSectionItem,
+      button: $pageSectionItemLink,
+      id,
+      section: $pageBody.querySelector(`[data-sectionfor="${id}"]`),
+    };
+  });
+  const showSectionByIndex = (indexToShow, switchFocus = false) => {
+    selectedIndex = indexToShow;
+    sections.forEach((section, index) => {
+      if (index === indexToShow) {
+        section.section.removeAttribute("hidden");
+        section.section.setAttribute("tabindex", "0");
+        section.button.setAttribute("tabindex", "0");
+        if (switchFocus) {
+          section.button.focus();
+        }
+        section.listItem.classList.add(
+          "etna-secondary-navigation__item--current",
+        );
+      } else {
+        section.section.setAttribute("hidden", "until-found");
+        section.section.setAttribute("tabindex", "-1");
+        section.button.setAttribute("tabindex", "-1");
+        section.listItem.classList.remove(
+          "etna-secondary-navigation__item--current",
+        );
+      }
+    });
+  };
+  const previousSection = () => {
+    if (selectedIndex >= 1) {
+      showSectionByIndex(selectedIndex - 1, true);
+    } else {
+      showSectionByIndex(sections.length - 1, true);
+    }
+  };
+  const nextSection = () => {
+    if (selectedIndex < sections.length - 1) {
+      showSectionByIndex(selectedIndex + 1, true);
+    } else {
+      showSectionByIndex(0, true);
+    }
+  };
+  sections.forEach((section, index) => {
+    section.section.setAttribute("role", "tabpanel");
+    section.section.setAttribute("aria-labelledby", section.button.id);
+    section.button.addEventListener("click", (e) => {
+      e.preventDefault();
+      showSectionByIndex(index, true);
+    });
+  });
+  showSectionByIndex(selectedIndex);
+  $secondaryNavigation.addEventListener("keydown", (e) => {
+    let preventDefaultKeyAction = false;
+    switch (e.key) {
+      case "ArrowLeft":
+      case "ArrowUp":
+        previousSection();
+        preventDefaultKeyAction = true;
+        break;
+      case "ArrowRight":
+      case "ArrowDown":
+        nextSection();
+        preventDefaultKeyAction = true;
+        break;
+      case "Home":
+        showSectionByIndex(0, true);
+        preventDefaultKeyAction = true;
+        break;
+      case "End":
+        showSectionByIndex(sections.length - 1, true);
+        preventDefaultKeyAction = true;
+        break;
+      default:
+        break;
+    }
+    if (preventDefaultKeyAction) {
+      e.stopPropagation();
+      e.preventDefault();
+    }
+  });
+}
