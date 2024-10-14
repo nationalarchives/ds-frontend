d27b81ae8232650d5fb71bc4010cde17e1451efb
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-10-14 18:03:37 +0100

    Add breadcrumbs to blog post pages

--- app/wagtail/pages/blog_post_page.py
+++ app/wagtail/pages/blog_post_page.py
@@ -1,3 +1,4 @@
+from app.wagtail.api import breadcrumbs
 from flask import render_template
 
 
@@ -5,4 +6,5 @@ def blog_post_page(page_data):
     return render_template(
         "blog/post.html",
         page_data=page_data,
+        breadcrumbs=breadcrumbs(page_data["id"]),
     )
