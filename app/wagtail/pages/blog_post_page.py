6056a5f66875bfc9b795e27042a1ae420b9bddbe
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-10-18 11:14:29 +0100

    Don't show blogs on a blog post

--- app/wagtail/pages/blog_post_page.py
+++ app/wagtail/pages/blog_post_page.py
@@ -3,21 +3,21 @@ from flask import current_app, render_template
 
 
 def blog_post_page(page_data):
-    try:
-        blogs_data = blogs()
-    except ConnectionError:
-        current_app.logger.error(
-            f"API error getting children for page {page_data['id']}"
-        )
-        return render_template("errors/api.html"), 502
-    except Exception:
-        current_app.logger.error(
-            f"Exception getting children for page {page_data['id']}"
-        )
-        return render_template("errors/server.html"), 500
+    # try:
+    #     blogs_data = blogs()
+    # except ConnectionError:
+    #     current_app.logger.error(
+    #         f"API error getting children for page {page_data['id']}"
+    #     )
+    #     return render_template("errors/api.html"), 502
+    # except Exception:
+    #     current_app.logger.error(
+    #         f"Exception getting children for page {page_data['id']}"
+    #     )
+    #     return render_template("errors/server.html"), 500
     return render_template(
         "blog/post.html",
         page_data=page_data,
-        blogs=blogs_data,
+        # blogs=blogs_data,
         breadcrumbs=breadcrumbs(page_data["id"]),
     )
