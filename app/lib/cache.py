c174ebfb175c78e830a92d60f36fb1ee87f57c43
Andrew Hosgood <mail@andrewhosgood.com>
2024-11-04 20:53:32 +0000

    Add dismissed_notifications to cache key prefix

--- app/lib/cache.py
+++ app/lib/cache.py
@@ -6,7 +6,7 @@ cache = Cache()
 
 def cache_key_prefix():
     """Make a key that includes GET parameters."""
-    return f"{request.full_path}{request.cookies.get('cookie_preferences_set' or '')}{request.cookies.get('theme' or '')}"
+    return f"{request.full_path}{request.cookies.get('cookie_preferences_set' or '')}{request.cookies.get('dismissed_notifications' or ''}{request.cookies.get('theme' or '')}"
 
 
 def rss_feedcache_key_prefix():
