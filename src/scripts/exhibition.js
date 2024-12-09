9a3b2d5d0b78ebc67a5d1635abffa568b67206ac
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-12-09 17:30:46 +0000

    Add maps to exhibition page

--- src/scripts/exhibition.js
+++ src/scripts/exhibition.js
@@ -1,1 +1,29 @@
-// console.log("Hello");
+import { Cookies } from "@nationalarchives/frontend/nationalarchives/all.mjs";
+
+const cookies = new Cookies();
+
+const enhanceMaps = () =>
+  document.querySelectorAll("[data-mapembedurl]").forEach(($mapReplacement) => {
+    const $mapIframe = document.createElement("iframe");
+    $mapIframe.classList.add("tna-enhanced-map");
+    $mapIframe.setAttribute("tabindex", "0");
+    $mapIframe.setAttribute("src", $mapReplacement.dataset["mapembedurl"]);
+    $mapReplacement.replaceWith($mapIframe);
+    const mapLink = $mapReplacement.dataset["maplink"];
+    if (mapLink) {
+      const $mapLink = document.createElement("p");
+      $mapLink.classList.add("tna-!--margin-top-xs");
+      $mapLink.innerHTML = `<a href="${mapLink}">See full map</a>`;
+      $mapIframe.after($mapLink);
+    }
+  });
+
+if (cookies.isPolicyAccepted("usage")) {
+  enhanceMaps();
+} else {
+  cookies.once("changePolicy", (policies) => {
+    if (policies["usage"]) {
+      enhanceMaps();
+    }
+  });
+}
