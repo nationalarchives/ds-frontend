e497fa328058af5d99294a4b8243d69283f75c23
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-12-10 12:02:08 +0000

    Disable maps in exhibition pages

--- src/scripts/exhibition.js
+++ src/scripts/exhibition.js
@@ -1,22 +1,22 @@
-import { Cookies } from "@nationalarchives/frontend/nationalarchives/all.mjs";
+// import { Cookies } from "@nationalarchives/frontend/nationalarchives/all.mjs";
 
-const cookies = new Cookies();
+// const cookies = new Cookies();
 
-const enhanceMaps = () =>
-  document.querySelectorAll("[data-mapembedurl]").forEach(($mapReplacement) => {
-    const $mapIframe = document.createElement("iframe");
-    $mapIframe.classList.add("tna-enhanced-map");
-    $mapIframe.setAttribute("tabindex", "0");
-    $mapIframe.setAttribute("src", $mapReplacement.dataset["mapembedurl"]);
-    $mapReplacement.replaceWith($mapIframe);
-    const mapLink = $mapReplacement.dataset["maplink"];
-    if (mapLink) {
-      const $mapLink = document.createElement("p");
-      $mapLink.classList.add("tna-!--margin-top-xs");
-      $mapLink.innerHTML = `<a href="${mapLink}">See full map</a>`;
-      $mapIframe.after($mapLink);
-    }
-  });
+// const enhanceMaps = () =>
+//   document.querySelectorAll("[data-mapembedurl]").forEach(($mapReplacement) => {
+//     const $mapIframe = document.createElement("iframe");
+//     $mapIframe.classList.add("tna-enhanced-map");
+//     $mapIframe.setAttribute("tabindex", "0");
+//     $mapIframe.setAttribute("src", $mapReplacement.dataset["mapembedurl"]);
+//     $mapReplacement.replaceWith($mapIframe);
+//     const mapLink = $mapReplacement.dataset["maplink"];
+//     if (mapLink) {
+//       const $mapLink = document.createElement("p");
+//       $mapLink.classList.add("tna-!--margin-top-xs");
+//       $mapLink.innerHTML = `<a href="${mapLink}">See full map</a>`;
+//       $mapIframe.after($mapLink);
+//     }
+//   });
 
 // if (cookies.isPolicyAccepted("usage")) {
 //   enhanceMaps();
