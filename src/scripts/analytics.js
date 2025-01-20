fd0938c14a263994cdf866c367cea03481c9f76d
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-01-20 15:42:08 +0000

    Add tracking event for CTA clicks

--- src/scripts/analytics.js
+++ src/scripts/analytics.js
@@ -30,12 +30,26 @@ if (ga4Id) {
     },
   ]);
 
-  analytics.addListeners(document.documentElement, "document", [
+  analytics.addListeners("body", "page", [
     {
       eventName: "double_click",
       on: "dblclick",
       data: {
         state: ($el, $scope, event) => helpers.getXPathTo(event.target),
+      },
+    },
+  ]);
+
+  analytics.addListeners("body", "page", [
+    {
+      eventName: "cta.click",
+      targetElement: ".tna-button[data-tna-cta]",
+      on: "click",
+      rootData: {
+        data_link: helpers.valueGetters.text,
+        data_component_name: "button",
+        data_link_type: "button",
+        event: "tna.select_cta",
       },
     },
   ]);
