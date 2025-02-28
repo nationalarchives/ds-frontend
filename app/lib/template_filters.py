45fbe4afbb1931c5df5dd580e3d3627b260ea2f5
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-02-28 12:06:36 +0000

    Handle KeyError when removing filter in qs_update

--- app/lib/template_filters.py
+++ app/lib/template_filters.py
@@ -337,6 +337,9 @@ def qs_toggler(existing_qs, filter, by):
 
 def qs_update(existing_qs, filter, value):
     rtn_qs = existing_qs.copy()
-    rtn_qs.pop(filter)
+    try:
+        rtn_qs.pop(filter)
+    except KeyError:
+        pass
     rtn_qs.update({filter: value})
     return urlencode(rtn_qs)
