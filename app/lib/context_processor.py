20185191a9a47244a53b08d8b532c2d8807aebb6
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-03-03 17:37:45 +0000

    Enhance pretty date range formatting

--- app/lib/context_processor.py
+++ app/lib/context_processor.py
@@ -32,7 +32,7 @@ def cookie_preference(policy):
     return None
 
 
-def get_date_from_string(s):
+def get_datetime_from_string(s):
     try:
         return datetime.strptime(s, "%Y-%m-%dT%H:%M:%SZ")
     except ValueError:
@@ -41,6 +41,10 @@ def get_date_from_string(s):
         return datetime.strptime(s, "%Y-%m-%dT%H:%M:%S")
     except ValueError:
         pass
+    return None
+
+
+def get_date_from_string(s):
     try:
         return datetime.strptime(s, "%Y-%m-%d")
     except ValueError:
@@ -71,10 +75,20 @@ def get_month_year_from_date_string(s):
 
 
 def pretty_date_range(s_from, s_to, show_days=True):
-    date_from = get_date_from_string(s_from)
-    date_to = get_date_from_string(s_to)
+    show_time = False
+    datetime_from = get_datetime_from_string(s_from)
+    datetime_to = get_datetime_from_string(s_to)
+    if datetime_from and datetime_to:
+        show_time = True
+        date_from = datetime_from
+        date_to = datetime_to
+    else:
+        date_from = get_date_from_string(s_from)
+        date_to = get_date_from_string(s_to)
     if date_from and date_to:
-        date_to_string = date_to.strftime("%-d %B %Y" if show_days else "%B %Y")
+        date_to_string = date_to.strftime(
+            "%-d %B %Y, %H:%M" if show_time else "%-d %B %Y" if show_days else "%B %Y"
+        )
         if (
             date_from.day == 1
             and date_from.month == 1
@@ -82,22 +96,42 @@ def pretty_date_range(s_from, s_to, show_days=True):
             and date_to.month == 12
         ):
             if date_from.year == date_to.year:
-                return str(date_from.year)
+                return (
+                    f"{date_from.strftime('%-d %B, %H:%M')} to {date_to_string}"
+                    if show_time
+                    else str(date_from.year)
+                )
             return f"{date_from.year}–{date_to.year}"
         if date_from.year == date_to.year:
             if date_from.month == date_to.month:
                 if date_from.day == date_to.day:
-                    return date_from.strftime("%-d %B %Y" if show_days else "%B %Y")
+                    return (
+                        f"{date_from.strftime('%-d %B %Y, %H:%M')}–{date_to.strftime('%H:%M')}"
+                        if show_time
+                        else date_from.strftime("%-d %B %Y" if show_days else "%B %Y")
+                    )
                 elif show_days:
-                    return f"{date_from.strftime('%-d')}–{date_to_string}"
+                    return (
+                        f"{date_from.strftime('%-d %B, %H:%M')} to {date_to_string}"
+                        if show_time
+                        else f"{date_from.strftime('%-d')}–{date_to_string}"
+                    )
                 else:
                     return date_to_string
             else:
-                return f"{date_from.strftime('%-d %B' if show_days else "%B")} to {date_to_string}"
+                return (
+                    f"{date_from.strftime('%-d %B, %H:%M')} to {date_to_string}"
+                    if show_time
+                    else f"{date_from.strftime('%-d %B' if show_days else "%B")} to {date_to_string}"
+                )
         else:
-            return f"{date_from.strftime('%-d %B %Y' if show_days else "%B %Y")} to {date_to_string}"
-    if date_from:
-        return f"From {date_from.strftime('%-d %B %Y' if show_days else "%B %Y")}"
-    if date_to:
-        return f"To {date_to.strftime('%-d %B %Y' if show_days else "%B %Y")}"
+            return (
+                f"{date_from.strftime('%-d %B %Y, %H:%M')} to {date_to_string}"
+                if show_time
+                else f"{date_from.strftime('%-d %B %Y' if show_days else "%B %Y")} to {date_to_string}"
+            )
+    if date_from or datetime_from:
+        return f"From {datetime_from.strftime("%-d %B %Y, %H:%M") if datetime_from else date_from.strftime("%-d %B %Y" if show_days else "%B %Y")}"
+    if date_to or datetime_to:
+        return f"To {datetime_to.strftime("%-d %B %Y, %H:%M") if datetime_to else date_to.strftime("%-d %B %Y" if show_days else "%B %Y")}"
     return f"{s_from}–{s_to}"
--- test/lib/test_context_processor.py
+++ test/lib/test_context_processor.py
@@ -7,6 +7,7 @@ from app.lib.context_processor import pretty_date_range
 class ContextParserTestCase(unittest.TestCase):
     def test_pretty_date_range(self):
         start_date = "2000-01-01"
+        self.assertEqual(pretty_date_range(start_date, ""), "From 1 January 2000")
         self.assertEqual(pretty_date_range(start_date, "2000-01-01"), "1 January 2000")
         self.assertEqual(
             pretty_date_range(start_date, "2000-01-02"), "1–2 January 2000"
@@ -24,6 +25,39 @@ class ContextParserTestCase(unittest.TestCase):
             "1 January 2000 to 1 January 2001",
         )
         self.assertEqual(pretty_date_range(start_date, "2001-12-31"), "2000–2001")
+
+    def test_pretty_date_range_with_time(self):
+        start_date = "2000-01-01T13:30:00"
+        self.assertEqual(
+            pretty_date_range(start_date, ""), "From 1 January 2000, 13:30"
+        )
+        self.assertEqual(
+            pretty_date_range(start_date, "2000-01-01T15:00:00"),
+            "1 January 2000, 13:30–15:00",
+        )
+        self.assertEqual(
+            pretty_date_range(start_date, "2000-01-02T15:00:00"),
+            "1 January, 13:30 to 2 January 2000, 15:00",
+        )
+        self.assertEqual(
+            pretty_date_range(start_date, "2000-01-31T15:00:00"),
+            "1 January, 13:30 to 31 January 2000, 15:00",
+        )
+        self.assertEqual(
+            pretty_date_range(start_date, "2000-02-01T15:00:00"),
+            "1 January, 13:30 to 1 February 2000, 15:00",
+        )
+        self.assertEqual(
+            pretty_date_range(start_date, "2000-12-31T15:00:00"),
+            "1 January, 13:30 to 31 December 2000, 15:00",
+        )
+        self.assertEqual(
+            pretty_date_range(start_date, "2001-01-01T15:00:00"),
+            "1 January 2000, 13:30 to 1 January 2001, 15:00",
+        )
+        self.assertEqual(
+            pretty_date_range(start_date, "2001-12-31T15:00:00"), "2000–2001"
+        )
 
     def test_pretty_date_range_no_days(self):
         start_date = "2000-01-01"
