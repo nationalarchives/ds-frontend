7b41b1f7a5d08602e77b9ecc7c0c139071981a7d
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-03-25 10:46:34 +0000

    Rename events_page function to event_listing_page

--- app/wagtail/pages/whatson/event_listing_page.py
+++ app/wagtail/pages/whatson/event_listing_page.py
@@ -2,7 +2,7 @@ from app.wagtail.api import breadcrumbs
 from flask import render_template
 
 
-def events_page(page_data):
+def event_listing_page(page_data):
     return render_template(
         "whats_on/event_listing_page.html",
         breadcrumbs=breadcrumbs(page_data["id"]),
