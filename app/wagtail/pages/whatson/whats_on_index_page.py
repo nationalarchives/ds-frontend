ec6c51aa6da9f6253c5d0433dbde5ab7327caea4
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-06-19 17:26:37 +0100

    Add more functionality to events pages

--- app/wagtail/pages/whatson/whats_on_index_page.py
+++ app/wagtail/pages/whatson/whats_on_index_page.py
@@ -1,8 +1,11 @@
+from app.wagtail.api import page_children
 from flask import render_template
 
 
 def whats_on_index_page(page_data):
+    children = page_children(page_data["id"])
     return render_template(
         "whats_on/index.html",
         page_data=page_data,
+        children=children.get("items", []),
     )
