9b40f0f7f53da5ff0c02c3ae011ff863db321de1
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-06-19 11:59:59 +0100

    Improve support for date ranges with time

--- test/lib/test_context_processor.py
+++ test/lib/test_context_processor.py
@@ -28,23 +28,72 @@ class ContextParserTestCase(unittest.TestCase):
     def test_pretty_date_range_no_days(self):
         start_date = "2000-01-01"
         self.assertEqual(
-            pretty_date_range(start_date, "2000-01-01", False), "January 2000"
+            pretty_date_range(start_date, "2000-01-01", ignore_days=False),
+            "January 2000",
         )
         self.assertEqual(
-            pretty_date_range(start_date, "2000-01-02", False), "January 2000"
+            pretty_date_range(start_date, "2000-01-02", ignore_days=False),
+            "January 2000",
         )
         self.assertEqual(
-            pretty_date_range(start_date, "2000-01-31", False), "January 2000"
+            pretty_date_range(start_date, "2000-01-31", ignore_days=False),
+            "January 2000",
         )
         self.assertEqual(
-            pretty_date_range(start_date, "2000-02-01", False),
+            pretty_date_range(start_date, "2000-02-01", ignore_days=False),
             "January to February 2000",
         )
-        self.assertEqual(pretty_date_range(start_date, "2000-12-31", False), "2000")
         self.assertEqual(
-            pretty_date_range(start_date, "2001-01-01", False),
+            pretty_date_range(start_date, "2000-12-31", ignore_days=False), "2000"
+        )
+        self.assertEqual(
+            pretty_date_range(start_date, "2001-01-01", ignore_days=False),
             "January 2000 to January 2001",
         )
         self.assertEqual(
-            pretty_date_range(start_date, "2001-12-31", False), "2000 to 2001"
+            pretty_date_range(start_date, "2001-12-31", ignore_days=False),
+            "2000 to 2001",
+        )
+
+    def test_pretty_date_range_with_time(self):
+        start_date = "2000-01-01T12:30:00Z"
+        self.assertEqual(
+            pretty_date_range(start_date, "2000-01-01T12:30:00Z", show_time=True),
+            "1 January 2000, 12:30",
+        )
+        self.assertEqual(
+            pretty_date_range(start_date, "2000-01-01T12:31:00Z", show_time=True),
+            "1 January 2000, 12:30 to 12:31",
+        )
+        self.assertEqual(
+            pretty_date_range(start_date, "2000-01-01T23:59:59Z", show_time=True),
+            "1 January 2000, 12:30 to 23:59",
+        )
+        self.assertEqual(
+            pretty_date_range(start_date, "2000-01-02T00:00:0Z", show_time=True),
+            "1 to 2 January 2000",
+        )
+        self.assertEqual(
+            pretty_date_range(start_date, "2000-01-02T14:45:00Z", show_time=True),
+            "1 to 2 January 2000",
+        )
+        self.assertEqual(
+            pretty_date_range(start_date, "2000-01-31T14:45:00Z", show_time=True),
+            "1 to 31 January 2000",
+        )
+        self.assertEqual(
+            pretty_date_range(start_date, "2000-02-01T14:45:00Z", show_time=True),
+            "1 January to 1 February 2000",
+        )
+        self.assertEqual(
+            pretty_date_range(start_date, "2000-12-31T14:45:00Z", show_time=True),
+            "2000",
+        )
+        self.assertEqual(
+            pretty_date_range(start_date, "2001-01-01T14:45:00Z", show_time=True),
+            "1 January 2000 to 1 January 2001",
+        )
+        self.assertEqual(
+            pretty_date_range(start_date, "2001-12-31T14:45:00Z", show_time=True),
+            "2000 to 2001",
         )
