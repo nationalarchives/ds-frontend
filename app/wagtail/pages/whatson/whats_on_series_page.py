eb59ccd36de5628192b42f80188f95b45f20ca83
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-07-14 14:27:52 +0100

    Update audience details to age details across event and exhibition templates; enhance series handling in series page

--- app/wagtail/pages/whatson/whats_on_series_page.py
+++ app/wagtail/pages/whatson/whats_on_series_page.py
@@ -6,4 +6,5 @@ def whats_on_series_page(page_data):
         "whats_on/events.html",
         page_data=page_data,
         events=page_data.get("event_listings", []),
+        exhibitions=page_data.get("exhibition_listings", []),
     )
