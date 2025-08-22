c0558272e3458e5951992d49b6c9e353c8412e0d
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-08-22 12:45:59 +0100

    Fix cache key prefixes

--- app/lib/cache.py
+++ app/lib/cache.py
@@ -12,11 +12,11 @@ def page_cache_key_prefix():
     keys = [
         request.url,
         # request.cookies.get("cookie_preferences_set", "false"),
-        request.cookies.get("dontShowCookieNotice", "0"),
+        # request.cookies.get("dontShowCookieNotice", "0"),
         request.cookies.get("theme", "0"),
     ]
     return "_".join(keys)
 
 
 def rss_feed_cache_key_prefix():
-    return f"{request.base_url}{request.cookies.get('format') or ''}"
+    return f"{request.base_url}{request.args.get('format', '')}"
