66c0be786d931703dae4900f622648047af65ff9
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-05-26 16:51:26 +0100

    Formatting updates

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,17 +3,5 @@ import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
   ...tnaEslintConfig,
-  {
-    rules: {
-      "capitalized-comments": "off",
-      "no-magic-numbers": "off",
-      "no-new": "off",
-      "no-ternary": "warn",
-      "one-var": "off",
-      "sort-imports": ["error", { ignoreDeclarationSort: true }],
-      "sort-keys": "off",
-      "sort-vars": "off",
-    },
-  },
   globalIgnores(["app/static/**/*", "webpack.config.js"]),
 ]);
