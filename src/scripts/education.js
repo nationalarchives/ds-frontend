d04db15e456bd45d60f008ea822de0a4435806b9
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-08-04 14:50:25 +0100

    Update fetch URL to use beta environment for teaching resources

--- src/scripts/education.js
+++ src/scripts/education.js
@@ -243,7 +243,12 @@ if (
 
       const fileBlobs = await Promise.all(
         files.map(async (file) => {
-          const response = await fetch(file.source);
+          const response = await fetch(
+            file.source.replace(
+              "www.nationalarchives.gov.uk",
+              "beta.nationalarchives.gov.uk",
+            ),
+          );
           if (!response.ok) {
             throw new Error("Network response was not ok");
           }
