49ef3368b142d170713b09dc46503ebaefa8ead9
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-08-05 17:14:21 +0100

    Formatting

--- app/lib/context_processor.py
+++ app/lib/context_processor.py
@@ -25,7 +25,9 @@ def now_rfc_822():
 
 def cookie_preference(policy):
     if current_app.config["COOKIE_PREFERENCES_KEY"] in request.cookies:
-        cookie_preferences = request.cookies[current_app.config["COOKIE_PREFERENCES_KEY"]]
+        cookie_preferences = request.cookies[
+            current_app.config["COOKIE_PREFERENCES_KEY"]
+        ]
         preferences = json.loads(unquote(cookie_preferences))
         return preferences.get(policy, None)
     return None
