96b7bf224a668ddaab5873990c760493afc3f44d
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-08-05 15:44:19 +0100

    Remove outdated abbreviations from the list

--- app/lib/abbreviations.py
+++ app/lib/abbreviations.py
@@ -1,11 +1,3 @@
 abbreviations = [
-    ("BBC", "British Broadcasting Corporation"),
-    ("MI5", "Military Intelligence, Section 5"),
-    ("PRO", "Public Record Office"),
     ("TNA", "The National Archives"),
-    ("HMRC", "His Majesty's Revenue and Customs"),
-    (
-        "ED 128",
-        "Committee for the Education of Poles in Great Britain (Gater Committee), and Ministry of Education, Polish Sections of Awards and External Relation Branches: Polish Resettlement Files",
-    ),
 ]
