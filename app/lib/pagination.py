bddc5769a5e585d275fb0b9515b3da0352767a5b
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-08-19 17:13:05 +0100

    Refactor pagination function to accept previous and next page properties as parameters

--- app/lib/pagination.py
+++ app/lib/pagination.py
@@ -1,8 +1,20 @@
 from tna_utilities.component import tna_frontend_pagination
 
 
-def pagination(qs, pages, page):
+def pagination(
+    qs, pages, page, previous_page_properties=None, next_page_properties=None
+):
     pagination = None
+
+    if previous_page_properties is None:
+        previous_page_properties = {
+            "title": "Previous page",
+        }
+
+    if next_page_properties is None:
+        next_page_properties = {
+            "title": "Next page",
+        }
 
     if pages:
         pagination_queries = qs.new().remove_parameter("page")
@@ -10,6 +22,12 @@ def pagination(qs, pages, page):
             base_pagination_url = pagination_queries.get_query_string() + "&page="
         else:
             base_pagination_url = "?page="
-        pagination = tna_frontend_pagination(pages, page, base_pagination_url)
+        pagination = tna_frontend_pagination(
+            pages,
+            page,
+            base_pagination_url,
+            previous_page_properties=previous_page_properties,
+            next_page_properties=next_page_properties,
+        )
 
     return pagination
