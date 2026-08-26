50ffb592ebef25ae4edc508d0e4f99d51f396d7f
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-08-26 21:38:25 +0100

    Add additional abbreviations for CSV, GCSE, and JSON

--- app/lib/abbreviations.py
+++ app/lib/abbreviations.py
@@ -1,3 +1,6 @@
 abbreviations = [
+    ("CSV", "Comma-Separated Values, a simple file format used to store tabular data"),
+    ("GCSE", "General Certificate of Secondary Education"),
+    ("JSON", "JavaScript Object Notation, a lightweight data-interchange format"),
     ("TNA", "The National Archives"),
 ]
