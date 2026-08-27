2c06777a29dd42f880f60afd3100f4e690b32029
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-08-27 13:08:46 +0100

    Add more abbreviations

--- app/lib/abbreviations.py
+++ app/lib/abbreviations.py
@@ -1,6 +1,15 @@
 abbreviations = [
+    (
+        "AQA",
+        "AQA Education (formerly the Assessment and Qualifications Alliance), an education and examination body",
+    ),
     ("CSV", "Comma-Separated Values, a simple file format used to store tabular data"),
+    ("EDEXCEL", "An education and examination body"),
     ("GCSE", "General Certificate of Secondary Education"),
     ("JSON", "JavaScript Object Notation, a lightweight data-interchange format"),
+    (
+        "OCR",
+        "Cambridge OCR (formerly Oxford Cambridge and RSA Examinations), an education and examination body",
+    ),
     ("TNA", "The National Archives"),
 ]
